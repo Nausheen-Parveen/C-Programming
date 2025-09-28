eb1ad2bd80d3f971d27c98c506c82a04504bc1db
NAUSHEEN PARVEEN <nausheenparveen940@gmail.com>
2025-09-29 02:37:43 +0530

    Update day_13.c

--- 11 to 20 codes/day_13.c
+++ 11 to 20 codes/day_13.c
@@ -5,17 +5,18 @@ int main() {
     char s[100];
     char sen[100];
 
-    // Read single character
+    
     scanf("%c", &ch);
     printf("%c\n", ch);
 
-    // Read a word (without spaces)
+    
     scanf("%s", s);
     printf("%s\n", s);
 
-    // Read a full sentence (with spaces)
+    
     scanf(" %[^\n]", sen);   // space before %[^\n] ignores leftover newline
     printf("%s\n", sen);
 
     return 0;
 }
+
